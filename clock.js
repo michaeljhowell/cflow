c045df4995163d5f6a46bd800fa6cd2d0d2f79f1
michael.j.howell@gmail.com <michael.j.howell@gmail.com>
2015-08-03 16:49:55 -0500

    Got magnets mostly working

--- clock.js
+++ clock.js
@@ -5,9 +5,29 @@ var paper = new joint.dia.Paper({
     width: 600,
     height: 600,
     model: graph,
-    gridSize: 1
+    gridSize: 1,
+    defaultLink: new joint.dia.Link({
+        smooth: true,
+        attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' } }
+    }),
+    validateConnection: function(cellViewS, magnetS, cellViewT, magnetT, end, linkView) {
+        // Prevent linking from input ports.
+        if (magnetS && magnetS.getAttribute('type') === 'input') return false;
+        // Prevent linking from output ports to input ports within one element.
+        if (cellViewS === cellViewT) return false;
+        // Prevent linking to input ports.
+        return magnetT && magnetT.getAttribute('type') === 'input';
+    },
+    snapLinks: { radius: 75 },
+    markAvailable: true
 });
 
+paper.on('cell:pointerup', function(cellView, event, x, y){
+    console.log("pointerup");
+    console.log(event);
+    console.log(x+" "+y);
+    console.log("1:"+cellView);
+});
 var nTime = new joint.shapes.basic.Rect({
     position: { x: 100, y: 30 },
     size: { width: 75, height: 75 },
@@ -24,7 +44,7 @@ nToggle.attr({
     text: {text:'Toggle'}
 });
 
-//nTime.on('change', function(){alert('Parent Changed')});
+nTime.on('change:z', function(){alert('Parent Changed')});
 
 var nHourHand = nTime.clone();
 nHourHand.attr({
@@ -41,7 +61,9 @@ nClockFace.attr({
     rect: {fill: 'white'},
     text: {text:'Clock Face'}
 });
+
 var nOutput = nTime.clone();
+
 nOutput.attr({
     rect: {fill: 'white'},
     text: {text:'Output'}
@@ -58,27 +80,41 @@ var link = new joint.dia.Link({
     source: { id: nTime.id },
     target: { id: nToggle.id }
 });
-link.set('smooth', true)
+
 var link2 = new joint.dia.Link({
     source: { id: nTime.id },
     target: { id: nHourHand.id }
 });
-link2.set('smooth', true)
+
 graph.addCells([nTime, nToggle, nHourHand, nMinuteHand,
     nClockFace, nOutput,
     link, link2]);
 
-
 var m1 = new joint.shapes.devs.Model({
-    position: { x: 50, y: 50 },
+    position: { x: 50, y: 75 },
     size: { width: 90, height: 90 },
-    inPorts: ['in1','in2'],
-    outPorts: ['out'],
+    inPorts: [],
+    outPorts: [''],
     attrs: {
-        '.label': { text: 'Model', 'ref-x': .4, 'ref-y': .2 },
-        rect: { fill: '#2ECC71' },
-        '.inPorts circle': { fill: '#16A085' },
+        '.label': { text: 'Model', 'ref-y': .4 },
+        rect: { fill: '#2ECC71', rx: 50, ry: 80 },
+        '.inPorts circle': { fill: '#16A085', magnet: 'passive', type: 'input' },
         '.outPorts circle': { fill: '#E74C3C' }
     }
-});
-graph.addCell(m1);
+}).addTo(graph);
+
+
+var m2 = new joint.shapes.devs.Model({
+    position:{x:200, y:100},
+    size: { width: 90, height: 90 },
+    inPorts: [''],
+    outPorts: [''],
+    attrs: {
+        rect: {rx: 0, ry: 0, fill: '#2ECC71'},
+        '.label': {text: 'Other Model'},
+        '.inPorts circle': { fill: '#0099FF', magnet: 'passive', type: 'input'},
+        '.outPorts circle': { fill: '#E74C3C' }
+    }
+}).addTo(graph);
+
+m2.prop("outPorts", ["we", "wee"]);
